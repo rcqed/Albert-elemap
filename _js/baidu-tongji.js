77030c4da389640b2ebd6af1ca8b04fb728f696f
rcqed <rcqed@outlook.com>
2023-10-01 23:00:13 +0800

    update

---
+++ _js/baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?a44b80b496dad613ff5414bf8344d64a";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
